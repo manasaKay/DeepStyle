e62a90153dfc87b4456c9f0ccfe8c2d6a628eec2
Srivaishnavi-Sree-Krishnan <51536680+Srivaishnavi-Sree-Krishnan@users.noreply.github.com>
2019-06-10 15:23:51 -0700

    Add files via upload

---
+++ Demo/utils.py
@@ -0,0 +1,47 @@
+import torch
+from PIL import Image
+
+
+def imread(filename, size=None, scale=None):
+    img = Image.open(filename)
+    if size is not None:
+        img = img.resize((size, size), Image.ANTIALIAS)
+    elif scale is not None:
+        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
+    return img
+
+
+def save_image(filename, data):
+    img = data.clone().clamp(0, 255).numpy()
+    img = img.transpose(1, 2, 0).astype("uint8")
+    img = Image.fromarray(img)
+    img.save(filename)
+
+
+def gram_matrix(y):
+    (b, ch, h, w) = y.size()
+    features = y.view(b, ch, w * h)
+    features_t = features.transpose(1, 2)
+    gram = features.bmm(features_t) / (ch * h * w)
+    return gram
+
+
+def normalize_imageset(batch):
+    # normalize using imagenet mean and std
+    mean = batch.new_tensor([0.485, 0.456, 0.406]).view(-1, 1, 1)
+    std = batch.new_tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
+    batch = batch.div_(255.0)
+    return (batch - mean) / std
+
+def get_resnet_features(model, image):
+    layers = set(['conv1','bn1','relu','maxpool','layer1','layer2', 'layer3', 'layer4'])
+    x = image
+    features = {}
+    for name, layer in model._modules.items():
+        if name == 'fc':
+            x = x.view(-1, 512)
+        x = layer(x)
+        if name in layers:
+            features[name] = x 
+        
+    return features
